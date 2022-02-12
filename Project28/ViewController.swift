874329754b171d0487dc18453e87fd9b511e291c
uzayaltiner <92029942+uzayaltiner@users.noreply.github.com>
2022-02-12 17:58:13 +0300

    Authetication function added

--- Project28/ViewController.swift
+++ Project28/ViewController.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Uzay Altıner on 11.02.2022.
 //
-
+import LocalAuthentication
 import UIKit
 
 class ViewController: UIViewController {
@@ -23,7 +23,27 @@ class ViewController: UIViewController {
     }
 
     @IBAction func authenticateTapped(_ sender: Any) {
-        unlockSecretMessage()
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "Identify yourself!"
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) {[weak self] success, authenticationError in
+                DispatchQueue.main.async {
+                    if success {
+                        self?.unlockSecretMessage()
+                    } else {
+                        let ac = UIAlertController(title: "Authentication Fail", message: "You could not be verified", preferredStyle: .alert)
+                        ac.addAction(UIAlertAction(title: "OK", style: .default))
+                        self?.present(ac, animated: true)
+                    }
+                }
+            }
+        } else {
+            let ac = UIAlertController(title: "Biometry unavaible", message: "Your device is not configured for biometric authentication.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        }
     }
     
     
